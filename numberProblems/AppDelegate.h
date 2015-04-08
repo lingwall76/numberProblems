5b3647839eacdf81b256b4546a3c3fcf09670200
sarah <lingwall76@gmail.com>
2015-04-08 14:55:47 -0600

    Initial Commit

---
+++ numberProblems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  numberProblems
+//
+//  Created by Sarah on 4/8/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
